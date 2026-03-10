caea15d88ee01e193a9d0fb8a9a54ef365bdf8c6
prp <aunkhwjai@gmail.com>
2026-03-10 17:37:29 +0700

    เพิ่ม Products หน้าหลัก

--- app/data/products1.ts
+++ app/data/products1.ts
@@ -1,26 +1,38 @@
 export const products = [
   {
     id: "fert-001",
-    name: "ปุ๋ยสูตร 16-16-16",
+    name: "ปุ๋ยอินทรีย์เคมี 12 - 3 - 5",
     price: 1290,
-    image: "/image/Fertilizer/3-6-15.png",
+    image: "/image/Fertilizer/1.jpg",
   },
   {
     id: "fert-002",
-    name: "ปุ๋ยสูตร 12-6-15",
+    name: "ปุ๋ยอินทรีย์เคมี 12 - 6 - 15",
     price: 1390,
-    image: "/image/Fertilizer/3-6-15.png",
+    image: "/image/Fertilizer/2.jpg",
   },
   {
     id: "fert-003",
-    name: "ปุ๋ยโพแทสเซียม 0-0-60",
-    price: 890,
-    image: "/image/Fertilizer/3-6-15.png",
+    name: "ปุ๋ยเคมี 0 - 0 - 30",
+    price: 1390,
+    image: "/image/Fertilizer/3.jpg",
   },
   {
     id: "fert-004",
-    name: "โดโลไมท์ปรับสภาพดิน",
-    price: 390,
-    image: "/image/Fertilizer/3-6-15.png",
+    name: "ปุ๋ยอินทรีย์ผง MO 25 %",
+    price: 690,
+    image: "/image/Fertilizer/4.jpg",
+  },
+  {
+    id: "fert-003",
+    name: "ปุ๋ยอินทรีย์เม็ด MO 20 %",
+    price: 790,
+    image: "/image/Fertilizer/5.jpg",
+  },
+  {
+    id: "fert-003",
+    name: "ปุ๋ยอินทรีเคมี 6 - 3 - 3",
+    price: 1090,
+    image: "/image/Fertilizer/6.jpg",
   }
 ];
