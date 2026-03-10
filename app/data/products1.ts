d46d8f1cdee0541b977315529a27ca7423501725
heng <henggamert@gmail.com>
2026-03-10 17:27:44 +0700

    save local changes

---
+++ app/data/products1.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: "fert-001",
+    name: "ปุ๋ยสูตร 16-16-16",
+    price: 1290,
+    image: "/image/Fertilizer/3-6-15.png",
+  },
+  {
+    id: "fert-002",
+    name: "ปุ๋ยสูตร 12-6-15",
+    price: 1390,
+    image: "/image/Fertilizer/3-6-15.png",
+  },
+  {
+    id: "fert-003",
+    name: "ปุ๋ยโพแทสเซียม 0-0-60",
+    price: 890,
+    image: "/image/Fertilizer/3-6-15.png",
+  },
+  {
+    id: "fert-004",
+    name: "โดโลไมท์ปรับสภาพดิน",
+    price: 390,
+    image: "/image/Fertilizer/3-6-15.png",
+  }
+];
