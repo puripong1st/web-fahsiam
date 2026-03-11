90e2706669fadcd3e946889909ab9282b186187b
prp <aunkhwjai@gmail.com>
2026-03-11 10:36:44 +0700

    ลบส่วนที่ไม่ใช้ออก

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Contact from "./contact/Contact"
 export default function RootLayout({
   children,
 }: {
@@ -12,8 +11,6 @@ export default function RootLayout({
       <body>
 
         <Navbar />
-        
-
         {children}
         <Footer />
 
